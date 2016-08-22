36df5be0715da16cb4c35cb7893ef96c13279fb9
Noah Spochart <noah@Noahs-iMac.home>
2016-08-22 00:20:04 -0400

    touch support

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,14 +1,24 @@
+/*****************************************
+ **  Simple fullpage Parallax Scroll Effect
+ **  with touch support
+ **  https://codepen.io/franzk/pen/aNxQxP
+ **
+ **  based on work by Emily Hayman
+ **  https://codepen.io/eehayman/pen/qdGZJr
+ **
+ *****************************************/
+
 // ------------- VARIABLES ------------- //
 var ticking = false;
 var isFirefox = (/Firefox/i.test(navigator.userAgent));
 var isIe = (/MSIE/i.test(navigator.userAgent)) || (/Trident.*rv\:11\./i.test(navigator.userAgent));
-var scrollSensitivitySetting = 50; //Increase/decrease this number to change sensitivity to trackpad gestures (up = less sensitive; down = more sensitive) 
+var scrollSensitivitySetting = 50; //Increase/decrease this number to change sensitivity to trackpad gestures (up = less sensitive; down = more sensitive)
 var slideDurationSetting = 600; //Amount of time for which slide is "locked"
 var currentSlideNumber = 0;
 var totalSlideNumber = $(".background").length;
 
 // ------------- DETERMINE DELTA/SCROLL DIRECTION ------------- //
-function parallaxScroll(evt) {
+function wheelScroll(evt) {
   if (isFirefox) {
     //Set delta for Firefox
     delta = evt.detail * (-120);
@@ -19,7 +29,31 @@ function parallaxScroll(evt) {
     //Set delta for all other browsers
     delta = evt.wheelDelta;
   }
+  if (ticking != true) {
+    if (delta <= -scrollSensitivitySetting) {
+      //Down scroll
+      ticking = true;
+      if (currentSlideNumber !== totalSlideNumber - 1) {
+        currentSlideNumber++;
+        nextItem();
+      }
+      slideDurationTimeout(slideDurationSetting);
+    }
+    if (delta >= scrollSensitivitySetting) {
+      //Up scroll
+      ticking = true;
+      if (currentSlideNumber !== 0) {
+        currentSlideNumber--;
+      }
+      previousItem();
+      slideDurationTimeout(slideDurationSetting);
+    }
+  }
+}
 
+function touchScroll(ts, te) {
+  delta = te - ts;
+  //console.log('para');
   if (ticking != true) {
     if (delta <= -scrollSensitivitySetting) {
       //Down scroll
@@ -51,7 +85,17 @@ function slideDurationTimeout(slideDuration) {
 
 // ------------- ADD EVENT LISTENER ------------- //
 var mousewheelEvent = isFirefox ? "DOMMouseScroll" : "wheel";
-window.addEventListener(mousewheelEvent, _.throttle(parallaxScroll, 60), false);
+var ts;
+
+window.addEventListener(mousewheelEvent, _.throttle(wheelScroll, 60), false);
+//window.addEventListener(mousewheelEvent, _.throttle(60, wheelScroll), false);
+window.addEventListener("touchstart", function(e) {
+  ts = e.touches[0].clientY;
+}, false);
+window.addEventListener("touchend", function(e) {
+  var te = e.changedTouches[0].clientY;
+  touchScroll(ts, te);
+}, false);
 
 // ------------- SLIDE MOTION ------------- //
 function nextItem() {
